e89b6084bf48d852db26fd474078e822bf9af35a
juanbruno99 <juan.marcos.bruno@gmail.com>
2021-03-20 16:57:24 +0000

    Fully functional working version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Spark Basic Transformations Example Project
 
-Project demonstrates usage of Basic Transformations (map, flatmap, etc.) in Spark Java Library.
+Project demonstrates usage of Basic Transformations and actions (map, filter, etc.) in Spark Java Library.
 
 To run execute:
 
--- src/main/java/com/jmb/BasicTransformations.java
+++ src/main/java/com/jmb/BasicTransformations.java
@@ -1,10 +1,16 @@
 package com.jmb;
 
+import com.jmb.filter.HerbsFilter;
+import com.jmb.mapper.EmptyToUnderscoreMapper;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 
 public class BasicTransformations {
@@ -33,11 +39,47 @@ public class BasicTransformations {
                 .option("header", "true")
                 .load(PATH_RESOURCES);
 
-        //Show first 5 records of the Raw ingested dataset
-        //df.show(5);
+        //Show first 5 records of the Raw ingested DataSet
+        df.show(5);
 
-        //Also print its schema
-        //df.printSchema();
+        //Apply a Map Transformation
+        Dataset<Row> transformedDf = applyMapTransformation(df);
 
+        //Also print first five rows
+        transformedDf.show(5);
+
+        //Filter food and keep ones belonging to the Herbs subgroup
+        Dataset<Row> filteredDf = applyFilerFunction(transformedDf);
+
+        //Print some rows to see filter was applied correctly
+        filteredDf.show(4);
+
+        //Apply an in-line predicate to filter column's values
+        Dataset<Row> inlinefilteredDf = transformedDf.filter(transformedDf.col("GROUP").equalTo("Vegetables"));
+        inlinefilteredDf.show(5);
+
+        //Count the total filtered rows for veggies group
+        LOGGER.info("Total vegetables in DataFrame: " + inlinefilteredDf.count());
+
+        //Take the first 4 veggies from the Filtered DataFrame
+        Row[] result = (Row[]) inlinefilteredDf.take(4);
+        //And print the Resulting array of Row
+        LOGGER.info("=============== Printing Rows =============== ");
+        Arrays.asList(result).stream().forEach(row -> LOGGER.info("Row: " + row.mkString(",")));
+        LOGGER.info("=============== End Printing Rows =============== ");
+
+        //Collect all the rows
+        Row[] collected = (Row[]) inlinefilteredDf.collect();
+        //Display the size of the Java list with the collected rows
+        LOGGER.info("Total Collected Rows for DataFrame: " + collected.length);
+
+    }
+
+    private Dataset<Row> applyMapTransformation(Dataset<Row> inputDf) {
+        return inputDf.map(new EmptyToUnderscoreMapper("GROUP"), RowEncoder.apply(inputDf.schema()));
+    }
+
+    private Dataset<Row> applyFilerFunction(Dataset<Row> inputDf) {
+        return inputDf.filter(new HerbsFilter());
     }
 }
